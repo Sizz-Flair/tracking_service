9376f863ec6075bc7d734b68a2576da1bf641e01
sizz <akfur222@naver.com>
2024-05-22 17:53:30 +0900

    Jenkins Test2

---
+++ src/main/java/com/giant/tracking/GlobalE2.java
@@ -0,0 +1,12 @@
+package com.giant.tracking;/**
+* packageName    : com.giant.tracking
+* fileName       : GlobalE2
+* author         : akfur
+* date           : 2024-05-22
+* description    : 
+*======================================================================
+* DATE             AUTHOR            NOTE
+*----------------------------------------------------------------------
+* 2024-05-22         akfur           
+*/public class GlobalE2 {
+}
